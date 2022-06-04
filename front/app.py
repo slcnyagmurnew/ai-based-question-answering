4c766177053fd64053ee076a525e904a479a7dc9
Yağmur Atak <l1119601@std.yildiz.edu.tr>
2022-06-04 15:19:27 +0300

    time calculation added

--- front/app.py
+++ front/app.py
@@ -11,6 +11,7 @@ from nlp.utils import clean_question, find_objects, list_to_string
 import speech_recognition as sr
 import torch
 from ip.demo import detect_cv2, color_result, object_result, object_count_result
+from datetime import datetime
 
 prediction_folder = os.path.join('static', 'images')
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -35,6 +36,10 @@ def speech_to_text():
         return text
 
 
+def get_current_time():
+    return datetime.now()
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -47,6 +52,8 @@ def index():
         text = speech_to_text()
         cl = clean_question(text)
 
+        start_time = get_current_time()
+        print(f'Starting the process..\nCurrent time: {start_time.strftime("%H:%M:%S")}')
         try:
             obj = find_objects(list_to_string(cl))
             obj = obj[0]
@@ -75,6 +82,13 @@ def index():
             result = object_result(all_list)
         else:
             result = color_result(all_list, object_name=obj)
+
+        finish_time = get_current_time()
+        print(f'Process finished..\nCurrent time: {finish_time.strftime("%H:%M:%S")}')
+        difference = finish_time - start_time
+        seconds_in_day = 24 * 60 * 60
+        print(divmod(difference.days * seconds_in_day + difference.seconds, 60))
+
         predicted_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'predictions.jpg')
         return render_template('index.html', cls=str(result), predicted_image=predicted_filename)
     else:
